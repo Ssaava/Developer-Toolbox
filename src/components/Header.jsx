26e58bd21d88e5a4c62b94d2b795c51e459310ce
Ema⚡ <107991182+Ssaava@users.noreply.github.com>
2023-10-22 00:49:31 +0300

    Worked with tailwind css for the first time

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,24 @@
+import { HiSearch, HiOutlineMenu } from "react-icons/hi";
+import { BsSun } from "react-icons/bs";
 const Header = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="px-8 py-6 flex justify-between items-center border-b">
+        <div className="">Logo</div>
+        <div className="flex justify-between border-2 items-center px-4 rounded-md bg-gray-600 py-3 w-5/12">
+          <input
+            className="outline-none text-white bg-transparent w-10/12"
+            type="search"
+            placeholder="search resources..."
+          />
+          <HiSearch />
+        </div>
+        <div className="flex justify-between items-center gap-6">
+          <HiOutlineMenu />
+          <BsSun />
+        </div>
+      </div>
+    </>
+  );
 };
 export default Header;
