fd61e6d36dd9d53451a0683b6023d0d070823192
Ema⚡ <107991182+Ssaava@users.noreply.github.com>
2023-11-15 00:54:47 +0300

    added new components

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,11 +3,11 @@ import { BsSun } from "react-icons/bs";
 const Header = () => {
   return (
     <>
-      <div className="px-8 py-6 flex justify-between items-center border-b">
+      <div className="px-8 py-6 flex justify-between items-center border-b w-full md:w-9/12 ms-auto bg-slate-400">
         <div className="">Logo</div>
-        <div className="flex justify-between border-2 items-center px-4 rounded-md bg-gray-600 py-3 w-5/12">
+        <div className="flex justify-between border-2 items-center px-4 rounded-md bg-gray-600 py-3 w-9/12 max-w-xl">
           <input
-            className="outline-none text-white bg-transparent w-10/12"
+            className="outline-none text-white text-base max-w-prose bg-transparent w-10/12"
             type="search"
             placeholder="search resources..."
           />
--- src/components/SideNav.jsx
+++ src/components/SideNav.jsx
@@ -1,5 +1,18 @@
+import { Button, Avatar } from "./utilities";
+
 const SideNav = () => {
-  return <div>hello worlds</div>;
+  return (
+    <div className="md:w-3/12 w-0 overflow-hidden p-5 bg-white absolute top-0">
+      <div className="profile ">
+        <Avatar />
+        <div>
+          <h2 className="m-auto w-fit">Name</h2>
+          <p className="m-auto w-fit text-stone-400">About info</p>
+        </div>
+        <Button />
+      </div>
+    </div>
+  );
 };
 
 export default SideNav;
