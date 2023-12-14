afedfb0c2d2906d5ffa05959d64007662cd0a8fa
Ema⚡ <107991182+Ssaava@users.noreply.github.com>
2023-12-14 23:30:17 +0300

    removed unneccessary comments

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,24 +1,8 @@
 import { Card, SearchInput } from "./utilities";
-// import ProgrammingLanguages from "../../data/ProgramingLanguages.json";
 import dataFiles from "../../public/data/index.tsx";
 
 import { useEffect, useState } from "react";
 
-// async function Test() {
-//   try {
-//     dataFiles.forEach(async (file) => {
-//       const response = await fetch(`../../public/data/${file}`);
-//       const data = await response.json();
-
-//       data.forEach((language) => {
-//         console.log(language);
-//       });
-//     });
-//   } catch (err) {
-//     console.error(err);
-//   }
-// }
-// Test();
 const Main = () => {
   const [cards, setCards] = useState([]);
   useEffect(() => {
@@ -41,30 +25,6 @@ const Main = () => {
       });
       setCards(cardsArray);
     });
-    // async function readJsonFiles() {
-    //   try {
-    //     const cardsArray = [];
-    //     dataFiles.forEach(async (file) => {
-    //       const response = await fetch(`../../data/${file}`);
-    //       const data = await response.json();
-    //       data.forEach((language) => {
-    //         cardsArray.push(
-    //           <Card
-    //             key={language.examples[0].name}
-    //             details={language.examples[0].description}
-    //             title={language.examples[0].name}
-    //             icon={language.examples[0].icon}
-    //             link={language.examples[0].link}
-    //           />
-    //         );
-    //       });
-    //       setCards(cardsArray);
-    //     });
-    //   } catch (e) {
-    //     console.log("Error reading fiels in the folder", e);
-    //   }
-    // }
-    // readJsonFiles();
   }, []);
 
   return (
